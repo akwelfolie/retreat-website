12f7161aea030da869509f48615fb7898b805ab0
Alex Petro <alexpetro@Alexs-MacBook-Pro.local>
2026-01-05 11:05:59 -0600

    add pronouns to all speakers

--- src/content/people.ts
+++ src/content/people.ts
@@ -37,14 +37,14 @@ export const founders: Person[] = [
     bio: "Nicole Sonnefeldt (she/her) is an alternative relationship coach & consultant & the founder of Freeform Coaching: Relationship Coaching for Conscious Non-Monogamy. A former trauma & couple's therapist with a Master's in Counseling Psychology from the Chicago School of Professional Psychology, she brings her experience and training in the psychology & physiology of trauma with her to Unleashed, along with her pleasure-positive, poly & kink-affirming stance as an alternative relationship coach. She assisted in curating the experiences and workshops at Unleashed with this background and knowledge, and looks forward to utilizing these skills to help create a trauma-informed, safe container for exploration and play for all presenters & attendees.",
   },
   {
-    name: 'Ashley Summers',
+    name: 'Ashley Summers (she/her)',
     role: 'Co-Founder',
     bigImage: bigAsh,
     squareImage: squareAsh,
     socials: [
       { name: 'instagram', href: 'https://www.instagram.com/ashleysummersphotography' },
     ],
-    bio: 'Ash Summers is a Chicago-based creative with a background in special events and certification as a recreation therapist. For the past 11 years she has run her photography business with Chicago as her home base. She believes in freedom of expression through photography, videography, and living out loud. She saw a need for others to have a safe space in a community setting to explore topics that she is passionate about including unapologetically going for what you want in all areas of life. Her work and ethos centers on connection, authenticity, and real human moments.',
+    bio: 'Ash Summers (she/her) is a Chicago-based creative with a background in special events and certification as a recreation therapist. For the past 11 years she has run her photography business with Chicago as her home base. She believes in freedom of expression through photography, videography, and living out loud. She saw a need for others to have a safe space in a community setting to explore topics that she is passionate about including unapologetically going for what you want in all areas of life. Her work and ethos centers on connection, authenticity, and real human moments.',
   },
 ]
 
@@ -59,7 +59,7 @@ export const speakers: Person[] = [
       { name: 'facebook', href: 'https://www.facebook.com/mistresssophia/' },
       { name: 'website', href: 'https://ladysophia.com/' },
     ],
-    bio: "Lady Sophia is a globally recognized professional dominatrix of 17 years, sex educator and kink lecturer, and the owner of Chicago Dungeon Rentals. She works with men, women, non-binary people, and couples in BDSM sessions designed to support exploration through trust, communication, and skilled, intentional play. Grounded in her Master of Social Work from the University of Chicago, her approach is risk-aware, consent-forward, and trauma-informed. Her expertise and years of experience have led her being featured on Showtime and Netflix, kink consulting roles on film projects, and guest lectures at Northwestern and other universities. She has also advocated for sex workers' rights in Washington, DC, and is internationally recognized for the quality of her client work.",
+    bio: "Lady Sophia (she/her) is a globally recognized professional dominatrix of 17 years, sex educator and kink lecturer, and the owner of Chicago Dungeon Rentals. She works with men, women, non-binary people, and couples in BDSM sessions designed to support exploration through trust, communication, and skilled, intentional play. Grounded in her Master of Social Work from the University of Chicago, her approach is risk-aware, consent-forward, and trauma-informed. Her expertise and years of experience have led her being featured on Showtime and Netflix, kink consulting roles on film projects, and guest lectures at Northwestern and other universities. She has also advocated for sex workers' rights in Washington, DC, and is internationally recognized for the quality of her client work.",
   },
   {
     name: 'Albe Gutierrez (Ms. Albe), MAATC (she/her/ella)',
@@ -70,17 +70,8 @@ export const speakers: Person[] = [
     ],
     bio: "Albe Gutierrez (she/her/ella), Ms. Albe, is a bilingual psychotherapist and art therapist at Chicago Therapy Collective with over a decade of clinical experience within hospitals, community organizations and substance use treatment centers and a decade more of teaching experience as a fine arts, movement and burlesque instructor. She maintains a focus on LGBTQ+ community needs and resourcing, particularly for other trans women of color by providing therapy, surgical readiness letter writing, and arts activism.",
   },
-  // {
-  //   name: 'Meg',
-  //   role: 'Rigger/Shibari Performer',
-  //   bigImage: imagePlaceholder,
-  //   squareImage: imageHeadshot,
-  //   socials: [
-  //   ],
-  //   bio: 'Short bio 2–3 sentences.',
-  // },
   {
-    name: 'Shanta “Nishi” Kanukollu, Ph. D',
+    name: 'Shanta “Nishi” Kanukollu, Ph. D (she/her)',
     role: 'Psychotherapist',
     bigImage: bigNishi,
     squareImage: squareNishi,
@@ -88,7 +79,18 @@ export const speakers: Person[] = [
       {name: 'instagram', href: 'https://www.instagram.com/snk_therapy'},
       {name: 'website', href: 'https://www.snktherapy.com/instagram'},
     ],
-    bio: 'Dr. K is a trauma-informed psychotherapist who works with individuals from diverse cultural backgrounds, with a focus on communities of color. She values therapy as a collaborative, relational process and strives to create a compassionate space where clients can explore vulnerability while recognizing their strengths. As a first-generation Indian-American woman, Dr. K brings a culturally responsive lens to her work and has a particular interest in supporting South Asian individuals navigating depression, anxiety, trauma, marginalization, and intergenerational stress.',
+    bio: 'Dr. K (she/her) is a trauma-informed psychotherapist who works with individuals from diverse cultural backgrounds, with a focus on communities of color. She values therapy as a collaborative, relational process and strives to create a compassionate space where clients can explore vulnerability while recognizing their strengths. As a first-generation Indian-American woman, Dr. K brings a culturally responsive lens to her work and has a particular interest in supporting South Asian individuals navigating depression, anxiety, trauma, marginalization, and intergenerational stress.',
+  },
+  {
+    name: 'Josephine Lauer (she/her)',
+    role: 'Somatic Intimacy Coach & Tantra Practitioner',
+    bigImage: bigJosephine,
+    squareImage: squareJosephineLauer,
+    socials: [
+      { name: 'instagram', href: 'https://www.instagram.com/templeofloha' },
+      { name: 'website', href: 'www.cur8chicago.com' },
+    ],
+    bio: "Josephine Lauer (she/her) is an Embodiment Guide, trauma-informed Somatic Intimacy Coach, Tantric Practitioner and Food Systems Consultant. She curates bold, brave, safe and sacred spaces for humans to explore their fullest, most authentic expressions, in order to achieve a more embodied existence via the harmonization of the mental, physical and emotional body. She is the founder of CUR8Chicago, a boutique consulting firm for embodied leaders, and Temple of Loha, an esoteric arts production house.  Josephine resides in Chicago, IL, is a Mother of 3 sons and travels often. Her podcast, Universal Intimacy, explores connections with the self, others, nature, and the cosmos via the intersection of Science, Spirituality and Sexuality.",
   },
   // {
   //   name: 'Dr. Kres Ashford, PsyD, LCPC, CST',
@@ -101,17 +103,15 @@ export const speakers: Person[] = [
   //   ],
   //   bio: 'Short bio 2–3 sentences.',
   // },
-  {
-    name: 'Josephine Lauer',
-    role: 'Somatic Intimacy Coach & Tantra Practitioner',
-    bigImage: bigJosephine,
-    squareImage: squareJosephineLauer,
-    socials: [
-      { name: 'instagram', href: 'https://www.instagram.com/templeofloha' },
-      { name: 'website', href: 'www.cur8chicago.com' },
-    ],
-    bio: "Josephine Lauer (she/her) is an Embodiment Guide, trauma-informed Somatic Intimacy Coach, Tantric Practitioner and Food Systems Consultant. She curates bold, brave, safe and sacred spaces for humans to explore their fullest, most authentic expressions, in order to achieve a more embodied existence via the harmonization of the mental, physical and emotional body. She is the founder of CUR8Chicago, a boutique consulting firm for embodied leaders, and Temple of Loha, an esoteric arts production house.  Josephine resides in Chicago, IL, is a Mother of 3 sons and travels often. Her podcast, Universal Intimacy, explores connections with the self, others, nature, and the cosmos via the intersection of Science, Spirituality and Sexuality.",
-  },
+  // {
+  //   name: 'Meg',
+  //   role: 'Rigger/Shibari Performer',
+  //   bigImage: imagePlaceholder,
+  //   squareImage: imageHeadshot,
+  //   socials: [
+  //   ],
+  //   bio: 'Short bio 2–3 sentences.',
+  // },
 ]
 
 // Getters (single source of truth)
